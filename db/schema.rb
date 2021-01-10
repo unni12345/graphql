09e67562949a37e0b9fa17514793dca98178b706
unni12345 <rajkrishnanv@gmail.com>
2021-01-10 21:48:59 +0530

    Association of user and links

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_151930) do
+ActiveRecord::Schema.define(version: 2021_01_10_161136) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_01_10_151930) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "links", "users"
 end
